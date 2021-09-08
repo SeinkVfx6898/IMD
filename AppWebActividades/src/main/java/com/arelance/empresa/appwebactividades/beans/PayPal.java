60db35207712755f33428d66b022bcf35dddf2c0
manuellibra <86914767+manulibra11996@users.noreply.github.com>
2021-09-08 12:49:10 +0200

    prueba subida-bajada

--- AppWebActividades/src/main/java/com/arelance/empresa/appwebactividades/beans/PayPal.java
+++ AppWebActividades/src/main/java/com/arelance/empresa/appwebactividades/beans/PayPal.java
@@ -11,5 +11,57 @@ package com.arelance.empresa.appwebactividades.beans;
  */
 public class PayPal {
 //    id_paypal, email, password, MetodoPago_id_MetodoPago
+    private int idPaypal;
+    private String email;
+    private String password;
+    private int MetodoPago_id_MetodoPago;
+
+    public PayPal(int idPaypal, String email, String password, int MetodoPago_id_MetodoPago) {
+        this.idPaypal = idPaypal;
+        this.email = email;
+        this.password = password;
+        this.MetodoPago_id_MetodoPago = MetodoPago_id_MetodoPago;
+    }
+
+    public PayPal(String email, String password, int MetodoPago_id_MetodoPago) {
+        this.email = email;
+        this.password = password;
+        this.MetodoPago_id_MetodoPago = MetodoPago_id_MetodoPago;
+    }
+
+    public int getIdPaypal() {
+        return idPaypal;
+    }
+
+    public void setIdPaypal(int idPaypal) {
+        this.idPaypal = idPaypal;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getMetodoPago_id_MetodoPago() {
+        return MetodoPago_id_MetodoPago;
+    }
+
+    public void setMetodoPago_id_MetodoPago(int MetodoPago_id_MetodoPago) {
+        this.MetodoPago_id_MetodoPago = MetodoPago_id_MetodoPago;
+    }
+    
+    
+    
     
 }
