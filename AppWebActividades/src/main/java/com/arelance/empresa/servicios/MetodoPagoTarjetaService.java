c16434fe945deb5348cb668ede55e653518505ae
manuellibra <86914767+manulibra11996@users.noreply.github.com>
2021-10-14 09:43:02 +0200

    DAO metodos hechos

---
+++ AppWebActividades/src/main/java/com/arelance/empresa/servicios/MetodoPagoTarjetaService.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arelance.empresa.servicios;
+
+
+
+import com.arelance.empresa.imd.domain.Metodopagotarjeta;
+import java.util.List;
+import javax.ejb.Local;
+
+/**
+ *
+ * @author Manuel
+ */
+@Local
+public interface MetodoPagoTarjetaService {
+    
+    public List<Metodopagotarjeta> obtener();//Obtener todos los metodos de pago por tarjeta
+    
+    public void AñadirPagoTarjeta(Metodopagotarjeta metodopagotarjeta);//Añadir un metodo de pago por tarjeta
+
+    public void ModificarPagoTarjeta(Metodopagotarjeta metodopagotarjeta);//Modificar un metodo de pago por tarjeta
+
+    public void RemoverPagoTarjeta(Metodopagotarjeta metodopagotarjeta);//Eliminar un metodo de pago por tarjeta
+    
+}
